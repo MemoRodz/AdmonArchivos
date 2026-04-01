014a09e696a57bf3eae06971b16da966118bf89f
MemoRodz <gmo.rodriguez@gmail.com>
2026-04-01 13:21:06 -0600

    Preprarando Contexto de Datos, servicio de correo para mensajes

---
+++ ArchivosMan/ArchivosMan.BLL/Interfaces/ICorreoService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArchivosMan.BLL.Interfaces
+{
+    public interface ICorreoService
+    {
+        Task<bool> EnviarCorreo(string CorreoDestino, string Asunto, string Mensaje);
+    }
+}
---
+++ ArchivosMan/ArchivosMan.BLL/Services/AuthState.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArchivosMan.BLL.Services
+{
+    internal class AuthState
+    {
+    }
+}
