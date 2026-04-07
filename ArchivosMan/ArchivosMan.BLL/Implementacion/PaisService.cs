7312aca322be2bb90a8d8ce76a17816cbadf7531
MemoRodz <gmo.rodriguez@gmail.com>
2026-04-06 22:56:36 -0600

    Servicio Pais

---
+++ ArchivosMan/ArchivosMan.BLL/Implementacion/PaisService.cs
@@ -0,0 +1,69 @@
+﻿
+
+using ArchivosMan.BLL.Interfaces;
+using ArchivosMan.DAL.DbContext;
+using ArchivosMan.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArchivosMan.BLL.Implementacion
+{
+    public class PaisService : IPaisService
+    {
+        private readonly ArchivosContext _db;
+
+        public PaisService(ArchivosContext db)
+        {
+            _db = db;
+        }
+        public async Task<Pais?> ActualizarAsync(Pais pais, int usuarioActualiza)
+        {
+            var existente = await _db.Pais.FirstOrDefaultAsync(p => p.IdPais == pais.IdPais);
+            if (existente == null) return null;
+
+            existente.Nombre = pais.Nombre;
+            existente.FechaActualiza = DateTime.Now;
+            existente.UsuarioActualiza = usuarioActualiza;
+
+            await _db.SaveChangesAsync();
+            return existente;
+        }
+
+        public async Task<Pais> CrearAsync(Pais pais, int usuarioCrea)
+        {
+            pais.EsActivo = true;
+            pais.FechaCrea = DateTime.Now;
+            pais.UsuarioCrea = usuarioCrea;
+
+            _db.Pais.Add(pais);
+            await _db.SaveChangesAsync();
+            return pais;
+        }
+
+        public async Task<bool> EliminarAsync(int id, int usuarioActualiza)
+        {
+            var pais = await _db.Pais.FirstOrDefaultAsync(p => p.IdPais == id);
+            if (pais == null) return false;
+
+            pais.EsActivo = false;
+            pais.FechaActualiza = DateTime.Now;
+            pais.UsuarioActualiza = usuarioActualiza;
+
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<List<Pais>> ListarAsync()
+        {
+            return await _db.Pais
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Pais?> ObtenerPorIdAsync(int id)
+        {
+            return await _db.Pais
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.IdPais == id);
+        }
+    }
+}
---
+++ ArchivosMan/ArchivosMan.BLL/Interfaces/IPaisService.cs
@@ -0,0 +1,14 @@
+﻿
+using ArchivosMan.Entity;
+
+namespace ArchivosMan.BLL.Interfaces
+{
+    public interface IPaisService
+    {
+        Task<Pais?> ActualizarAsync(Pais pais, int usuarioActualiza);
+        Task<Pais> CrearAsync(Pais pais, int usuarioCrea);
+        Task<bool> EliminarAsync(int id, int usuarioActualiza);
+        Task<List<Pais>> ListarAsync();
+        Task<Pais?> ObtenerPorIdAsync(int id);
+    }
+}
