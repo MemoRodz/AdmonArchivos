aab31ce7ddf64d77c273a54f971a2dd4bbcbda1b
MemoRodz <gmo.rodriguez@gmail.com>
2026-04-10 21:30:45 -0600

    Se agrega en Convertidor la encriptación de Texto

--- ArchivosMan/ArchivosMan.Web/Components/Pages/Convertidor.razor.cs
+++ ArchivosMan/ArchivosMan.Web/Components/Pages/Convertidor.razor.cs
@@ -6,14 +6,8 @@ namespace ArchivosMan.Web.Components.Pages
 {
     public partial class Convertidor
     {
-        private readonly IUtilidadesService? _utilidadesService;
-        [Inject]
-        public IUtilidadesService? utilidadesService
-        {
-            get => _utilidadesService;
-
-            init => _utilidadesService = value;
-        }
+        [Inject] private IUtilidadesService UtilidadesService { get; set; } = default!;
+        [Inject] private ICryptoService CryptoService { get; set; } = default!;
 
         private double valorMonto = 0.0;
         private bool mostrarDecimales;
@@ -21,10 +15,12 @@ namespace ArchivosMan.Web.Components.Pages
         private decimal temperaturaEntrada = 0.0m;
         private string unidadEntrada = "C"; 
         private decimal temperaturaSalida = 0.0m;
+        private string entrada = string.Empty;
+        private string salida = string.Empty;
 
         private void NumeroLetra()
         {
-            valorEnLetras = _utilidadesService.ConvertirNumeroALetras(valorMonto, mostrarDecimales);
+            valorEnLetras = UtilidadesService.ConvertirNumeroALetras(valorMonto, mostrarDecimales);
         }
 
         private void ConvertNumLetraOnKeyDownHandler(KeyboardEventArgs e)
@@ -44,15 +40,15 @@ namespace ArchivosMan.Web.Components.Pages
 
         private void ConvertirTemperatura()
         {
-            if (_utilidadesService is null) return;
+            if (UtilidadesService is null) return;
 
             switch (unidadEntrada.ToUpper())
             {
                 case "C":
-                    temperaturaSalida = _utilidadesService.fahrenheit(temperaturaEntrada);
+                    temperaturaSalida = UtilidadesService.fahrenheit(temperaturaEntrada);
                     break;
                 case "F":
-                    temperaturaSalida = _utilidadesService.celsiusF(temperaturaEntrada);
+                    temperaturaSalida = UtilidadesService.celsiusF(temperaturaEntrada);
                     break;
                 default:
                     temperaturaSalida = 0.0m; break;
@@ -78,5 +74,63 @@ namespace ArchivosMan.Web.Components.Pages
             }
         }
 
+        private void OnKeyDownHandlerCadenaEncriptada(KeyboardEventArgs e)
+        {
+            if (e.Key == "Enter")
+            {
+                ObtenerCadena();
+            }
+        }
+
+        private void ObtenerCadena()
+        {
+            if (string.IsNullOrWhiteSpace(entrada))
+            {
+                salida = $"Cadena vacía.";
+                return;
+            }
+
+            try
+            {
+                salida = $"Cadena cifrada AES:\n{CryptoService.Encrypt(entrada)}";
+                entrada = string.Empty; // Limpiar la entrada después de cifrar
+            }
+            catch (Exception ex)
+            {
+                salida = $"Ocurrió un error al cifrar la cadena: {ex.Message}";
+            }
+        }
+
+        private void LimpiarCadena()
+        {
+            entrada = string.Empty;
+            salida = string.Empty;
+        }
+
+        private void OnKeyDownHandlerCadenaDesencriptada(KeyboardEventArgs e)
+        {
+            if (e.Key == "Enter")
+            {
+                ResolverCadena();
+            }
+        }
+
+        private void ResolverCadena()
+        {
+            if (string.IsNullOrWhiteSpace(entrada))
+            {
+                salida = $"Cadena vacía.";
+                return;
+            }
+            try
+            {
+                salida = $"Cadena descifrada AES:\n{CryptoService.Decrypt(entrada)}";
+                entrada = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                salida = $"Ocurrió un error al descifrar la cadena: {ex.Message}";
+            }
+        }
     }
 }
